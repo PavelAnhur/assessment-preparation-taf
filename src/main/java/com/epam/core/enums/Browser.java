f144ebb801cc7ec56d816f1fff64b67f83140db2
Pavel Anhur <pavel_anhur@epam.com>
2021-09-28 21:01:40 +0300

    AP-TAF - Browser enum is enabled

--- src/main/java/com/epam/core/enums/Browser.java
+++ src/main/java/com/epam/core/enums/Browser.java
@@ -4,16 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum Browser {
-    CHROME("chrome"),
-    FIREFOX("firefox"),
-    OPERA("opera"),
-    REMOTE_CHROME("remoteChrome"),
-    REMOTE_FIREFOX("remoteFirefox"),
-    REMOTE_OPERA("remoteOpera");
-
-    private final String value;
-
-    Browser(String value) {
-        this.value = value;
-    }
+    CHROME,
+    FIREFOX,
+    OPERA,
+    REMOTECHROME,
+    REMOTEFIREFOX
 }
